36790b653175e707087ea992e52f5000a1f55d58
Sara Tateno <sara.tateno@gmail.com>
2015-11-14 08:05:37 +0900

    FizzBuzz pairing exercise complete

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,10 @@
 class Fizzbuzz
+
+
+  def self.play(n)
+    return 'FizzBuzz' if n % 3 == 0 && n % 5 == 0
+    return 'Fizz' if n % 3 == 0
+    return 'Buzz' if n % 5 == 0
+    return n
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,25 @@ describe Fizzbuzz do
   it 'returns fizz when given 3' do
     expect(Fizzbuzz.play(3)).to eq 'Fizz'
   end
+
+  it 'returns buzz when given 5' do
+    expect(Fizzbuzz.play(5)).to eq 'Buzz'
+  end
+
+  it 'returns FizzBuzz when given 15' do
+    expect(Fizzbuzz.play(15)).to eq 'FizzBuzz'
+  end
+
+  it 'returns 1 when given 1' do
+    expect(Fizzbuzz.play(1)).to eq 1
+  end
+
+  it 'returns fizz when divisible by 3' do
+    expect(Fizzbuzz.play(6)).to eq 'Fizz'
+  end
+
+  it 'returns buzz when divisible by 5' do
+    expect(Fizzbuzz.play(10)).to eq 'Buzz'
+  end
+
 end
